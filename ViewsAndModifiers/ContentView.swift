56e7e98a1eef5637fdcb94473e7c66b1bc41e2fb
karma <karmatashi954@gmail.com>
2022-05-06 13:59:28 +0545

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by karma on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by karma on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
